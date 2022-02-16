e934ed0b5bce39a0850806af3b750098c6ff11e1
coldu333 <97028490+coldu333@users.noreply.github.com>
2022-02-16 20:36:52 +0900

    2022_02_16_C

---
+++ 22_02_16_C/CStudy.c
@@ -0,0 +1,23 @@
+//구조체 변수의 초기화
+#include <stdio.h>
+
+struct point
+{
+    int xpos;
+    int ypos;
+};
+
+struct person
+{
+    char name[20];
+    char phoneNum[20];
+    int age;
+};
+
+int main()
+{
+    struct point pos = {10, 20};
+    struct person man = {"이승기", "010-1212-0001", 21};
+    printf("%d %d \n", pos.xpos, pos.ypos);
+    printf("%s %s %d \n", man.name, man.phoneNum, man.age);
+}
