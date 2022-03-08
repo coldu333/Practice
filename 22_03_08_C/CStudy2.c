7f6849c6dffb18320bb86f02cb8a34c370137a6b
coldu333 <97028490+coldu333@users.noreply.github.com>
2022-03-08 21:32:15 +0900

    22_03_08_C

---
+++ 22_03_08_C/CStudy2.c
@@ -0,0 +1,16 @@
+//조건부 컴파일 #else
+#include <stdio.h>
+#define HIT_NUM 7
+
+int main()
+{
+#if HIT_NUM==5
+    puts("매크로 상수 HIT_NUM은 현재 5입니다.");
+#elif HIT_NUM == 6
+    puts("매크로 상수 HIT_NUM은 현재 6입니다.");
+#elif HIT_NUM == 7
+    puts("매크로 상수 HIT_NUM은 현재 7입니다.");
+#else
+    puts("매크로 상수 HIT_NUM은 5,6,7이 아닙니다.");
+#endif
+}
